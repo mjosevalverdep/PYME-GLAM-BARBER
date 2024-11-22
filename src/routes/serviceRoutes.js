6ba5232dd8e8513ec639a481139f25e520f9908a
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-22 11:41:01 -0600

    feat: change files

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const serviceController = require('../controllers/ServiceController');
+const serviceController = require('../controllers/serviceController');
 
 router.post('/', serviceController.createService);
 router.get('/', serviceController.getServices);
